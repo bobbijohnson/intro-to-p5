d881dc7aab36f4b2e47fda00f347e8fa2aeb6ebd
Bobbi Johnson <bjohnson@wlapcs.org>
2017-03-01 18:48:47 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup(){
+    createCanvas(600, 400);
+    background(96, 95, 96);
+}
+
+function draw() {
+    strokeWeight(8);
+    fill(25,6,40); 
+    line(150, 100, 90, 495); 
+    line(10,300,400,350);
+    
+    
+    fill(96, 19, 47);
+    rect(450,50,50,50);
+    rect(450,95,75,75);
+    ellipse(300,70,60,89)
+    
+    stroke(5,0,1);
+    fill(126, 101, 142);
+    ellipse(50,250,80,80);
+    ellipse(190,70,80,90);
+    rect(95,300,80,80)
+    
+}
